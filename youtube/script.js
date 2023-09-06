9ffc44b52ca50ca82a2f1f74c2626c8ba93fe807
Naresh Chandragiri <nareshpittu769@gmail.com>
2023-09-06 23:51:38 +0530

    video content added

---
+++ youtube/script.js
@@ -0,0 +1,6 @@
+let hamburger = document.querySelector('.sidelines')
+let sidebar = document.querySelector('.side-bar')
+
+hamburger.addEventListener('click',()=>{
+    sidebar.classList.toggle('small-container')
+})
