245a18ae2328c2ec51fdb91f6196c68df09de9c0
Naresh Chandragiri <nareshpittu769@gmail.com>
2023-09-07 12:45:58 +0530

    responsive and images added

--- youtube/script.js
+++ youtube/script.js
@@ -1,6 +1,8 @@
 let hamburger = document.querySelector('.sidelines')
 let sidebar = document.querySelector('.side-bar')
+let bigcontainer = document.querySelector('.video-components')
 
 hamburger.addEventListener('click',()=>{
     sidebar.classList.toggle('small-container')
+    bigcontainer.classList.toggle('big-container')
 })
